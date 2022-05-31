25acce4ce3788bd627a7526a62495a5000a4a9a8
muranakar <p1211065@gmail.com>
2022-06-01 08:47:46 +0900

    初期設定

---
+++ PickerViewSample2022-06-01/mura/muraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  muraViewController.swift
+//  PickerViewSample2022-06-01
+//
+//  Created by 村中令 on 2022/06/01.
+//
+
+import UIKit
+
+class muraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PickerViewSample2022-06-01/sena/SenaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SenaViewController.swift
+//  PickerViewSample2022-06-01
+//
+//  Created by 村中令 on 2022/06/01.
+//
+
+import UIKit
+
+class SenaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
